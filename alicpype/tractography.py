55344d22f557da6cbcf66ee7a5ce86d505744cf7
Henry Braun <hbraun@umn.edu>
2023-06-16 13:57:03 -0500

    fix file exists checks, allow existing indata dir

--- alicpype/tractography.py
+++ alicpype/tractography.py
@@ -7,25 +7,26 @@ from warnings import warn
 from pathlib import Path
 from subprocess import run
 
+from . import config
+
 def generate_alic(cwd):
     cwd = Path(cwd)
-    to_check = [ parcellationPath, refT1Path, lutPath, diffPath, bvalsPath, bvecsPath]
+    to_check = [ config.parcellationPath, config.refT1Path, config.lutPath, 
+        config.diffPath, config.bvalsPath, config.bvecsPath]
     for i in to_check:
-        print(i)
-        if not i.is_file():
-            warn('%s doesn''t exist!' % str(i))
-
+        abs_file = cwd / i
+        if i.is_file():
+            print(f'found {abs_file}')
+        else:
+            warn('%s doesn''t exist!' % str(abs_file))
 
     # link indata to app-track_aLIC/indata
-    src = '../indata' 
-    dst = cwd/'app-track_aLIC/indata'
-    os.symlink(src, dst)
+    try:
+        os.symlink('../indata' , cwd/'app-track_aLIC/indata')
+    except FileExistsError:
+        # if app-track_aLic/indata already exists, ignore the error
+        pass
 
     # run "main" app-track_aLIC script (command to run, environment, select cwd)
     env = {**os.environ, "APPTAINER_BIND": '/home'}
-    run( './main', env = env, cwd = cwd/'app-track_aLIC')
-
-
-
-
-
+    run( './main', env=env, cwd=cwd/'app-track_aLIC')
