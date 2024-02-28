f61aa15d1d8d34daaadfe60cc2cfc29b68fd5c3d
Karianne Sretavan-Wong <sreta001@amit.cmrr.umn.edu>
2024-02-28 13:20:08 -0600

    script clean up config.py and tasks.py

--- alicpype/tasks.py
+++ alicpype/tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# description: module runs through entire alicpype for a single subject
+# description: runs through all subject-specific processing steps
 
 from pathlib import Path
 
@@ -8,54 +8,22 @@ from .externalio import import_hcp_subject, import_7T_hcp_subject, import_ocd_su
 from .tractography import generate_alic
 from .splitracc import split_racc
 from .subsegment import subsegment_alic
-# from .heatmap import generate_heatmap
 from .centroids import generate_centroid
-# from .slicer import generate_slicer
 
 def run_hcp_subject(subject, hcp_root, alicpype_root, selection=None ):
-    
-    #TODO:modify as necessary
     """ 
-    Run the pipeline steps "a la carte". Each step can be turned on or off.
-    Available steps (in order of execution) are:
-        '7T_convert':   Convert dicoms in Dicom/sub-{subject}/ses-7T to 
-                        BIDS-compatible NIFTIs
-        'CT_convert':   Convert dicoms in Dicom/sub-{subject}/ses-{CT_session}
-                        to BIDS-compatible NIFTIS for each CT session.
-        'Diffusion':    Run legacy DiffusionAnalysisTopup script
-        'T1_proc':      Run legacy HCP-based T1 processing
-        '7T_bet':       Run brain extraction on 7T T2 and T1
-        'STN_segmentation'  Run automated STN segmentation
-        'GP_segmentation'   Run automated GP segmentation
-        '7T_T2_reg':    Register 7T T2s to 7T T1 and apply to any segmentations
-                        in T2 spaces.
-        'apply_7T_reg': Transform segmentations from 7T T2 
-                        space(s) to 7T T1
-        '7T_swi_reg':   (naming is slightly broken, do this manually) Register 7T SWIs to 7T T1
-        'apply_swi_reg': Transform segmentations from 7T SWI space(s) to 7T T1
-        '3T_bet':       Run brain extraction on 3T T1 #TODO add other 3T scans
-        '7T_3T_reg':    (unavailable) Register 7T T1 to 3T T1
-        'ct_preproc':   Run CT preprocessing (ctmask, hbins)
-        'electrode':    Estimate electrode location
-        '7T_ct_reg':    Register 7T T1 to CT session(s)
-        'apply_7T_ct_reg': Transform images and segmentations from 7T to 
-                        CT space.
-        '3T_ct_reg':    Register 3T T1 to CT session(s)
-        'full_scene':   Build full scene, with all available coregistered data
-        'lite_scene':   (unavailable) Build 'lite' scene, with only 7T T1, 
-                        electrodes, and target
+    This function runs through all subject-specific processing steps for 3T HCP data. The available steps (in order of execution) are:
+    import_hcp_subject:     copy inputs from 3T HCP dataset
+    generate_alic:          generate whole ALIC tractogram
+    split_racc:             split rACC into dorsal and ventral components
+    subsegment_alic:        anatomically-based segmentation of the ALIC
+    generate_centroid:      calculate centroids from each segmented ALIC density map
 
-    :param subject:     Subject ID to process.
-    :type subject:      str
-    :param dbs_root:    Path of DBS data folder containing Dicom, Nifti, 
-                        and dbspype subfolders.
-    :type dbs_root:     String or Path-like
-    :param electrode:   Electrode model. Default is 'auto', but is not 
-                        recommended as this takes much longer to run 
-                        and sometimes guesses wrong.
-    :type electrode:    string, Optional.
-    :param selection:   Pieces of pipeline to run. if a dict is TODO
+    :subject:               subject ID
+    :hcp_root:              path to HCP-style dataset
+    :alicpype_root:         path to processed dataset from ALIC_tractography pipeline
     """
+
     hcp_root = Path(hcp_root)
 
     # copy and paste data into input folders (externalio.py)
@@ -64,82 +32,78 @@ def run_hcp_subject(subject, hcp_root, alicpype_root, selection=None ):
     cwd = alicpype_root / subject / 'OCD_pipeline'
     import_hcp_subject(subject,hcp_root,cwd)
 
-    # register images into acpc space (registration.py)
-    # data already registered
-    # register_acpc()
-
     # generate whole ALIC tractography (tractography.py)
     generate_alic(cwd)
 
-    # # generate split rACC ROI (divideracc.py)
+    # generate split rACC ROI (divideracc.py)
     split_racc(cwd)
 
-    # # subsegment ALIC based on PFC ROIs (subsegment.py)
+    # subsegment ALIC based on PFC ROIs (subsegment.py)
     subsegment_alic(cwd)
 
-    # # generate subsegmented heatmaps (heatmap.py)
-    # #generate_heatmap(subject, cwd)
-
-    # # calculate centroids of each subsegmented ALIC heatmap (centoids.py)
+    # calculate centroids of each subsegmented ALIC heatmap (centroids.py)
     generate_centroid(cwd)
 
-    # # generate a slicer scene containing heatmaps and centroids (slicer.py)
-    # generate_slicer(subject, cwd)
-
 def run_7T_hcp_subject (subject, hcp_root, alicpype_root, selection=None):
+    """ 
+    This function runs through all subject-specific processing steps for 7T HCP data. The available steps (in order of execution) are:
+    import_7T_hcp_subject:      copy inputs from 7T HCP dataset
+    generate_alic:              generate whole ALIC tractogram
+    split_racc:                 split rACC into dorsal and ventral components
+    subsegment_alic:            anatomically-based segmentation of the ALIC
+    generate_centroid:          calculate centroids from each segmented ALIC density map
+
+    :subject:                   subject ID
+    :hcp_root:                  path to HCP-style dataset
+    :alicpype_root:             path to processed dataset from ALIC_tractography pipeline
+    """
+    
     # copy and paste data into input folders (externalio.py)
     alicpype_root = Path(alicpype_root) #convert datatype to path
     subject = str(subject)
     cwd = alicpype_root / subject / 'OCD_pipeline'
     import_7T_hcp_subject(subject,hcp_root,cwd)
 
-    # register images into acpc space (registration.py)
-    # data already registered
-    # register_acpc()
-
     # generate whole ALIC tractography (tractography.py)
     generate_alic(cwd)
 
-    # # generate split rACC ROI (divideracc.py)
+    # generate split rACC ROI (divideracc.py)
     split_racc(cwd)
 
-    # # subsegment ALIC based on PFC ROIs (subsegment.py)
+    # subsegment ALIC based on PFC ROIs (subsegment.py)
     subsegment_alic(cwd)
 
-    # # generate subsegmented heatmaps (heatmap.py)
-    # #generate_heatmap(subject, cwd)
-
-    # # calculate centroids of each subsegmented ALIC heatmap (centoids.py)
+    # calculate centroids from each segmented ALIC density map (centroids.py)
     generate_centroid(cwd)
 
-    # # generate a slicer scene containing heatmaps and centroids (slicer.py)
-    # generate_slicer(subject, cwd)
-
 def run_ocd_subject (subject, input_data_root, alicpype_root, selection=None):
+    """ 
+    This function runs through all subject-specific processing steps for 7T data from OCD patients. The available steps (in order of execution) are:
+    import_ocd_subject:         copy inputs from 7T OCD dataset
+    generate_alic:              generate whole ALIC tractogram
+    split_racc:                 split rACC into dorsal and ventral components
+    subsegment_alic:            anatomically-based segmentation of the ALIC
+    generate_centroid:          calculate centroids from each segmented ALIC density map
+
+    :subject:                   subject ID
+    :input_data_root:           path to OCD patient dataset
+    :alicpype_root:             path to processed dataset from ALIC_tractography pipeline
+    """
+
     # copy and paste data into input folders (externalio.py)
     alicpype_root = Path(alicpype_root) #convert datatype to path
     subject = str(subject)
     cwd = alicpype_root / subject / 'OCD_pipeline'
     import_ocd_subject(subject,input_data_root,cwd)
 
-    # register images into acpc space (registration.py)
-    # data already registered
-    register_acpc()
-
     # generate whole ALIC tractography (tractography.py)
     generate_alic(cwd)
 
-    # # generate split rACC ROI (divideracc.py)
+    # generate split rACC ROI (divideracc.py)
     split_racc(cwd)
 
-    # # subsegment ALIC based on PFC ROIs (subsegment.py)
+    # subsegment ALIC based on PFC ROIs (subsegment.py)
     subsegment_alic(cwd)
 
-    # # generate subsegmented heatmaps (heatmap.py)
-    # #generate_heatmap(subject, cwd)
-
-    # # calculate centroids of each subsegmented ALIC heatmap (centoids.py)
+    # calculate centroids of each subsegmented ALIC heatmap (centroids.py)
     generate_centroid(cwd)
-
-    # # generate a slicer scene containing heatmaps and centroids (slicer.py)
-    # generate_slicer(subject, cwd)
